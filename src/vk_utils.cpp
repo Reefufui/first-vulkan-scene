d9443036a52b39c3a286611ad737ea6f0fa1fcc9
vfrolov <vova@frolov.pp.ru>
2019-11-25 22:39:40 +0300

    edit

--- src/vk_utils.cpp
+++ src/vk_utils.cpp
@@ -433,12 +433,12 @@ VkSurfaceFormatKHR ChooseSwapSurfaceFormat(const std::vector<VkSurfaceFormatKHR>
 
 VkPresentModeKHR ChooseSwapPresentMode(const std::vector<VkPresentModeKHR>& availablePresentModes) 
 {
-  for (const auto& availablePresentMode : availablePresentModes) {
-    if (availablePresentMode == VK_PRESENT_MODE_MAILBOX_KHR) {
+  for (const auto& availablePresentMode : availablePresentModes) 
+  {
+    if (availablePresentMode == VK_PRESENT_MODE_MAILBOX_KHR)
       return availablePresentMode;
     else if (availablePresentMode == VK_PRESENT_MODE_IMMEDIATE_KHR)
       return availablePresentMode;
-    }
   }
 
   return VK_PRESENT_MODE_FIFO_KHR;
